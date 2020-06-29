a8e9324b75ce4d5770612c87496c9f68f9cd3114
Krishnanshu Gupta <30324213+Krishnanshu-Gupta@users.noreply.github.com>
2020-06-29 16:21:58 -0700

    Update mobileSchedule.js

--- scripts/mobileSchedule.js
+++ scripts/mobileSchedule.js
@@ -17,9 +17,21 @@ $(document).ready(function() {
       toggleSelectDay();
       $("#day1-btn").addClass("active");
       $("#day2-btn").removeClass("active");
-
+      $("#day3-btn").addClass("active");
+      $("#day4-btn").removeClass("active");
+      $("#day5-btn").addClass("active");
+      $("#day6-btn").removeClass("active");
+      $("#day7-btn").addClass("active");
+      $("#day8-btn").removeClass("active");
+      
       $("#day1-pane").addClass("active");
       $("#day2-pane").removeClass("active");
+      $("#day3-pane").addClass("active");
+      $("#day4-pane").removeClass("active");
+      $("#day5-pane").addClass("active");
+      $("#day6-pane").removeClass("active");
+      $("#day7-pane").addClass("active");
+      $("#day8-pane").removeClass("active");
     }
   });
 
@@ -28,9 +40,21 @@ $(document).ready(function() {
       toggleSelectDay();
       $("#day1-btn").removeClass("active");
       $("#day2-btn").addClass("active");
+      $("#day3-btn").removeClass("active");
+      $("#day4-btn").addClass("active");
+      $("#day5-btn").removeClass("active");
+      $("#day6-btn").addClass("active");
+      $("#day7-btn").removeClass("active");
+      $("#day8-btn").addClass("active");
 
       $("#day1-pane").removeClass("active");
       $("#day2-pane").addClass("active");
+      $("#day3-pane").removeClass("active");
+      $("#day4-pane").addClass("active");
+      $("#day5-pane").removeClass("active");
+      $("#day6-pane").addClass("active");
+      $("#day7-pane").removeClass("active");
+      $("#day8-pane").addClass("active");
     }
   });
 });
